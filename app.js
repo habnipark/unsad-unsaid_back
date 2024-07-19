f1a79da00a1371ff4f6035e8aef4ad82c24849de
habin <habnipark@gmail.com>
2024-07-20 06:23:10 +0900

    CORS setting

--- app.js
+++ app.js
@@ -6,7 +6,21 @@ const chatbotRoutes = require('./routes/chatbot');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors()); // CORS 미들웨어 추가
+
+// CORS 설정
+const corsOptions = {
+  origin: [
+    "https://port-0-unsad-unsaid-back-lyt7bu192d19b53c.sel4.cloudtype.app",
+    "https://web-unsad-unsaid-front-lyt7bu192d19b53c.sel4.cloudtype.app",
+    "http://localhost:3000",
+    "http://127.0.0.1:5500",
+  ], // 허용할 도메인들
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // 허용할 HTTP 메서드
+  credentials: true, // 쿠키 허용 여부
+  optionsSuccessStatus: 204, // 사전 요청에 대한 성공 상태 코드
+};
+
+app.use(cors(corsOptions)); // CORS 미들웨어 추가
 
 app.use('/chatbot', chatbotRoutes);
 
