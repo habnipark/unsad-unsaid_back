4a5032331c090d7f64a8ed7cfea02257fc8fc108
habnipark <58678085+habnipark@users.noreply.github.com>
2024-08-17 11:35:56 +0900

    수정

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-require('dotenv').config(); // .env 파일의 환경 변수를 로드
+const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const chatbotRoutes = require('./routes/chatbot');
+const chatbotRoutes = require('./routes/chatbot.js');
+
+const frontUrl = `http://127.0.0.1:5500`;
+
+// .env 파일의 환경 변수를 로드합니다.
+dotenv.config();
+const apiKey = process.env.OPENAI_API_KEY;
+
+console.log('app', apiKey);
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors()); // CORS 미들웨어 추가
+
+// CORS 설정을 추가하여 특정 주소에서의 요청만 허용합니다.
+const corsOptions = {
+    origin: `${frontUrl}`,
+    optionsSuccessStatus: 200 // 일부 브라우저에서 CORS 프리플라이트 요청의 성공 상태를 204 대신 200으로 반환하도록 설정
+};
+
+app.use(cors(corsOptions));
+
+module.exports = { app, apiKey };
 
 app.use('/chatbot', chatbotRoutes);
 
